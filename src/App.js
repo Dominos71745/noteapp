9d3444aa99f6bbedf1f432f43458fcfa1f0fdd46
Dominos71745 <domistu@poczta.fm>
2023-08-02 23:58:05 +0200

    render note

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
+  const [notes, setNotes] = useState([]);
 
   const titleChangeHandler = (event) => {
     setTitle(event.target.value);
@@ -13,11 +14,13 @@ function App() {
     setDescription(event.target.value);
   };
 
-  let notes = {};
-
   const formHandler = (event) => {
     event.preventDefault();
     console.log(title, description);
+
+    setNotes((prevNotes) => [...prevNotes, { title, description }]);
+    setTitle("");
+    setDescription("");
   };
 
   return (
@@ -25,15 +28,31 @@ function App() {
       <form onSubmit={formHandler} className="header">
         <h1>Note App</h1>
         <label>Title</label>
-        <input type="text" id="title" onChange={titleChangeHandler}></input>
+        <input
+          type="text"
+          id="title"
+          onChange={titleChangeHandler}
+          value={title}
+        ></input>
         <label>Description</label>
         <input
           type="text"
           id="description"
           onChange={descriptionChangeHandler}
+          value={description}
         ></input>
         <button>Add Note</button>
       </form>
+      <div className="note-list">
+        {notes.map((note, index) => (
+          <div className="note" key={index}>
+            <section>
+              <li>{note.title}</li>
+              <li>{note.description}</li>
+            </section>
+          </div>
+        ))}
+      </div>
     </React.Fragment>
   );
 }
