7f2297628b280fece398577784fad8f77cd49ea7
Dominos71745 <domistu@poczta.fm>
2023-08-02 23:12:27 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.target.value);
+  };
+
+  let notes = {};
+
+  const formHandler = (event) => {
+    event.preventDefault();
+    console.log(title, description);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <form onSubmit={formHandler} className="header">
+        <h1>Note App</h1>
+        <label>Title</label>
+        <input type="text" id="title" onChange={titleChangeHandler}></input>
+        <label>Description</label>
+        <input
+          type="text"
+          id="description"
+          onChange={descriptionChangeHandler}
+        ></input>
+        <button>Add Note</button>
+      </form>
+    </React.Fragment>
   );
 }
 
